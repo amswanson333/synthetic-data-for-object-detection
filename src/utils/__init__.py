2f3c0d0a7ba749cb86dd7d8cc422bfaaed247801
amswanson333 <andrew.m.swanson.333@gmail.com>
2025-08-04 16:41:53 -0400

    added package variables for directories and imports

---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+
+RAW_DATA_DIR = 'data\\raw'
+INTERIM_DATA_DIR = 'data\\interim'
+PROCESSED_DATA_DIR = 'data\\processed'
+METADATA_DIR = 'data\\metadata'
+EXTERNAL_DATA_DIR = 'data\\external'
+
+from utils.label_data import *
+from utils.video_data import *
