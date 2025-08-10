71f01b74e7059bba7312f49f548b368242febddf
amswanson333 <andrew.m.swanson.333@gmail.com>
2025-08-10 16:20:34 -0400

    adjusted file ext logic to .lower()

--- src/utils/files.py
+++ src/utils/files.py
@@ -2,12 +2,11 @@ import os
 
 def get_video_files(directory):
 
-    video_extensions = ['.mp4', '.avi', '.mov', '.mkv', '.mpg',
-                        '.MP4', '.AVI', '.MOV', '.MKV', '.MPG']
+    video_extensions = ['.mp4', '.avi', '.mov', '.mkv', '.mpg']
     video_files = []
 
     for filename in os.listdir(directory):
-        if os.path.splitext(filename)[1] in video_extensions:
+        if os.path.splitext(filename)[1].lower() in video_extensions:
             name = os.path.splitext(filename)[0]
             video_files.append(name)
             
