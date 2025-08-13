969fbda0633f4779858dab9fa5f7e009b601d3f4
amswanson333 <andrew.m.swanson.333@gmail.com>
2025-08-12 21:12:12 -0400

    fixed the imput for the is_blank function

--- src/utils/image_data.py
+++ src/utils/image_data.py
@@ -6,13 +6,13 @@ def get_image_files(directory):
     file_ext = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
     image_files = []
     for file in os.listdir(directory):
-        if file.lower().endswith(file_ext):
+        if os.path.splitext(file)[1].lower() in file_ext:
             image_files.append(os.path.join(directory, file))
     return image_files
 
-def is_blank_image(directory, image, threshold=10):
+def is_blank_image(image_path, threshold=10):
     
-    img = cv2.imread(os.path.join(directory, image))
+    img = cv2.imread(image_path)
     if img is None:
         return False
     return (img < threshold).all()
