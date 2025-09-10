3ef451be828fef4d94186305674884b38a544fd5
amswanson333 <andrew.m.swanson.333@gmail.com>
2025-09-09 23:02:37 -0400

    moved get_image_files to files.py util

--- src/utils/image_data.py
+++ src/utils/image_data.py
@@ -2,17 +2,6 @@ import os
 import cv2
 from PIL import Image
 
-def get_image_files(directory):
-    '''
-    Get a list of image files in a directory.
-    '''
-    file_ext = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
-    image_files = []
-    for file in os.listdir(directory):
-        if os.path.splitext(file)[1].lower() in file_ext:
-            image_files.append(os.path.join(directory, file))
-    return image_files
-
 def is_blank_image(image_path, threshold=10):
     '''
     Check if an image is blank (all pixels below a certain threshold).
