002221d0d114d3dd0d338eb79a320b92979c0fe7
amswanson333 <andrew.m.swanson.333@gmail.com>
2025-09-09 23:02:07 -0400

    added minor docstrings moved get_image_files function here

--- src/utils/files.py
+++ src/utils/files.py
@@ -1,7 +1,9 @@
 import os
 
 def get_video_files(directory):
-
+    '''
+    Get a list of video files in a directory.
+    '''
     video_extensions = ['.mp4', '.avi', '.mov', '.mkv', '.mpg']
     video_files = []
 
@@ -14,3 +16,14 @@ def get_video_files(directory):
 def file_contains_name(file_path, test_names: list):
 
     return any(test_name in os.path.basename(file_path) for test_name in test_names)
+
+def get_image_files(directory):
+    '''
+    Get a list of image files in a directory.
+    '''
+    file_ext = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
+    image_files = []
+    for file in os.listdir(directory):
+        if os.path.splitext(file)[1].lower() in file_ext:
+            image_files.append(os.path.join(directory, file))
+    return image_files
