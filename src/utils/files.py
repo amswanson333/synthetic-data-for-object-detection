00e6dda9db52444b1859dd5ad28ca7177d0c3e2c
amswanson333 <andrew.m.swanson.333@gmail.com>
2025-10-27 10:51:38 -0400

    added functions to stage data temporarily

--- src/utils/files.py
+++ src/utils/files.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def get_video_files(directory):
     '''
@@ -49,3 +50,35 @@ def get_3d_model_files(directory):
         if os.path.splitext(file)[1].lower() in file_ext:
             model_files.append(os.path.join(directory, file))
     return model_files
+
+def copy_to_staging(df, stage='train'):
+    '''
+    Copy files listed in the dataframe to the staging directory.
+    '''
+    for idx, row in df.iterrows():
+        src_image_path = row['img_path']
+        src_label_path = row['ann_path']
+        
+        # Define destination paths
+        dest_image_path = os.path.join('data', 'staging', stage, os.path.basename(src_image_path))
+        dest_label_path = os.path.join('data', 'staging', stage, os.path.basename(src_label_path))
+
+        # Copy image and label files to staging directory
+        shutil.copy(src_image_path, dest_image_path)
+        shutil.copy(src_label_path, dest_label_path)
+        
+def cleanup_staging():
+    '''
+    Delete all files in the staging directories.
+    '''
+    staging_dirs = [os.path.join('data', 'staging', 'train'), os.path.join('data', 'staging', 'val')]
+    for staging_dir in staging_dirs:
+        for filename in os.listdir(staging_dir):
+            file_path = os.path.join(staging_dir, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f'Failed to delete {file_path}. Reason: {e}')
